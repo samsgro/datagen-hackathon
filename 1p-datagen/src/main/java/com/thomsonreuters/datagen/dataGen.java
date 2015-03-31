ad675257afbbb18b5b70f3ba8f71f62999a1c35f
ssgro <sam.sgro@thomsonreuters.com>
2015-03-31 14:31:18 -0700

    Adding arguments

--- 1p-datagen/src/main/java/com/thomsonreuters/datagen/dataGen.java
+++ 1p-datagen/src/main/java/com/thomsonreuters/datagen/dataGen.java
@@ -6,11 +6,60 @@ import java.io.OutputStreamWriter;
 
 import org.apache.wink.json4j.JSONException;
 import org.apache.wink.json4j.OrderedJSONObject;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.spi.BooleanOptionHandler;
 
 public class dataGen {
+	
+	@Option(name="-patents",usage="number of patents")
+    private int numPatents = 1;
+	
+	@Option(name="-literature",usage="number of literature records")
+    private int numLiterature = 1;
 
   public static void main(String[] args) throws IOException, JSONException, org.apache.wink.json4j.JSONException {
-    String patentStr   = "{"+"'tabName' : 'Patents'," 
+      new dataGen().doMain(args);
+  }
+
+private void doMain(String[] args) throws JSONException, IOException {
+	CmdLineParser parser = new CmdLineParser(this);
+    
+    // if you have a wider console, you could increase the value;
+    // here 80 is also the default
+    parser.setUsageWidth(80);
+	
+	try {
+        // parse the arguments.
+        parser.parseArgument(args);
+
+        // you can parse additional arguments if you want.
+        // parser.parseArgument("more","args");
+
+        // after parsing arguments, you should check
+        // if enough arguments are given.
+        //if( arguments.isEmpty() )
+       //     throw new CmdLineException(parser,"No argument is given");
+
+    } catch( CmdLineException e ) {
+        // if there's a problem in the command line,
+        // you'll get this exception. this will report
+        // an error message.
+        System.err.println(e.getMessage());
+        System.err.println("java SampleMain [options...] arguments...");
+        // print the list of available options
+        parser.printUsage(System.err);
+        System.err.println();
+
+
+        return;
+    }
+	
+	
+	
+	String patentStr   = "{"+"'tabName' : 'Patents'," 
                           + "'id' : 'UID'," 
                           + "'patNum':'USGRANTPATENT'," 
                           + "'pubdate': 'DATE',"
@@ -30,9 +79,9 @@ public class dataGen {
                           + "'refPatent':'USGRANTPATENT'," 
                           + "'abstract': 'SENTENCE'," 
                           + "}";
-    genData(patentStr, 5,"USGRANTPATENT");
-    genData(litStr, 5,"LITDOC");
-  }
+    genData(patentStr, numPatents,"USGRANTPATENT");
+    genData(litStr, numLiterature,"LITDOC");
+}
   
   public static void genData(String jsonStr, int num, String type) throws org.apache.wink.json4j.JSONException, IOException {
     OrderedJSONObject jsonObj = new OrderedJSONObject(jsonStr);
