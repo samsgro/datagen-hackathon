8ba909441c3fdbb98896bce8d44fd40df7e41fe9
ssgro <sam.sgro@thomsonreuters.com>
2015-03-31 13:27:59 -0700

    first commit

---
+++ graph-gen/src/main/java/com/thomsonreuters/graph/GraphstreamTest.java
@@ -0,0 +1,194 @@
+package com.thomsonreuters.graph;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.commons.lang3.StringUtils;
+import org.graphstream.algorithm.generator.BaseGenerator;
+import org.graphstream.algorithm.generator.Generator;
+import org.graphstream.algorithm.generator.RandomGenerator;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.MultiGraph;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.stream.file.FileSinkImages;
+import org.graphstream.stream.file.FileSinkImages.LayoutPolicy;
+import org.graphstream.stream.file.FileSinkImages.OutputType;
+import org.graphstream.stream.file.FileSinkImages.Resolution;
+import org.graphstream.stream.file.FileSinkImages.Resolutions;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.spi.BooleanOptionHandler;
+
+import com.thomsonreuters.graph.generator.TwoWayExclusiveRandomGenerator;
+
+public class GraphstreamTest {
+
+	
+	static String ABATTR = "distribution";
+	static String IDENTIFIERATTR = "identifier";
+	
+	@Option(name="-c",usage="connectedness")
+    private double connectedness = 1.2;
+	
+	@Option(name="-n",usage="repeat <n> cycles")
+    private int num = 0;
+	
+	@Option(name="-setA",usage="filename of set A identifiers")
+    private String setAFile;
+	
+	@Option(name="-setB",usage="filename of set B identifiers")
+    private String setBFile;
+	
+	 	@Argument
+	    private List<String> arguments = new ArrayList<String>();
+
+	    public static void main(String[] args) throws IOException {
+	        new GraphstreamTest().doMain(args);
+	    }
+	
+
+	public void doMain(String[] args) throws IOException {
+		CmdLineParser parser = new CmdLineParser(this);
+        
+        // if you have a wider console, you could increase the value;
+        // here 80 is also the default
+        parser.setUsageWidth(80);
+		
+		try {
+            // parse the arguments.
+            parser.parseArgument(args);
+
+            // you can parse additional arguments if you want.
+            // parser.parseArgument("more","args");
+
+            // after parsing arguments, you should check
+            // if enough arguments are given.
+            //if( arguments.isEmpty() )
+           //     throw new CmdLineException(parser,"No argument is given");
+
+        } catch( CmdLineException e ) {
+            // if there's a problem in the command line,
+            // you'll get this exception. this will report
+            // an error message.
+            System.err.println(e.getMessage());
+            System.err.println("java SampleMain [options...] arguments...");
+            // print the list of available options
+            parser.printUsage(System.err);
+            System.err.println();
+
+
+            return;
+        }
+		
+
+
+		System.out.println("-c was "+connectedness);
+		if(StringUtils.isEmpty(setAFile)) { System.out.println("must provide set A filename of identifiers"); return; }
+		if(StringUtils.isEmpty(setBFile)) { System.out.println("must provide set B filename of identifiers"); return; }
+		
+		Graph graph = new MultiGraph("Random");
+		String[] setAIdentifierArray = convertFileArrayToString(setAFile);
+		String[] setBIdentifierArray = convertFileArrayToString(setBFile);
+		
+		if (num == 0) num = (setAIdentifierArray.length + setBIdentifierArray.length) * 2;
+		
+		System.out.println("-n was "+num);
+		BaseGenerator gen = new TwoWayExclusiveRandomGenerator(connectedness, true, false, "distance", false, false, 
+				new Double ("0.50"), setAIdentifierArray, setBIdentifierArray);
+		gen.addSink(graph);
+		gen.setNodeAttributesRange(0, 1);
+		gen.begin();
+		for(int i=0; i<num; i++)
+		    gen.nextEvents();
+		gen.end();
+		
+		
+		outputCSVFile(graph, "output.csv");
+		outputScreenshot(graph, "screenshot.png");
+
+
+
+		
+	}
+	
+	
+	private void outputScreenshot(Graph graph, String output) throws IOException {
+
+		graph.addAttribute("ui.stylesheet", 
+				"edge { size: 1px; shape: line; fill-mode: dyn-plain; fill-color: #E6E6E6; arrow-size: 1px, 1px; z-index: 0;} "
+				+ "node { size: 3px; fill-color: #777;text-mode: normal; z-index: 5;} "
+				+ "node.isSetB { size: 3px; fill-color:red; z-index: 5;}");
+		graph.addAttribute("ui.quality");
+		graph.addAttribute("ui.antialias");
+		
+		OutputType type = OutputType.PNG;
+		Resolution resolution = Resolutions.TwoK;
+		FileSinkImages fsi = new FileSinkImages(type, resolution);
+		fsi.setLayoutPolicy(LayoutPolicy.COMPUTED_ONCE_AT_NEW_IMAGE);
+		fsi.writeAll(graph, output);
+		
+	}
+
+
+	private void outputCSVFile(Graph graph, String output) throws IOException {
+		File filename = new File(output);
+		
+		if (!filename.exists()) {
+			filename.createNewFile();
+		}
+		FileWriter fw = new FileWriter(filename);
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		for(Edge edge: graph.getEachEdge()) {
+			bw.write(edge.getNode0().getAttribute(IDENTIFIERATTR).toString()
+					+","
+					+edge.getNode1().getAttribute(IDENTIFIERATTR).toString());
+			bw.newLine();
+			
+		}
+		bw.flush(); bw.close(); fw.close();
+		
+	}
+
+
+	public static double round(double value, int places) {
+	    if (places < 0) throw new IllegalArgumentException();
+
+	    BigDecimal bd = new BigDecimal(value);
+	    bd = bd.setScale(places, RoundingMode.HALF_UP);
+	    return bd.doubleValue();
+	}
+	
+	private String[] convertFileArrayToString(String filename) throws FileNotFoundException, IOException {
+		 	
+		String token1 = "";
+		@SuppressWarnings("resource")
+		BufferedReader in = new BufferedReader(new FileReader(new File(filename)));
+
+		    List<String> temps = new ArrayList<String>();
+		    for (String x = in.readLine(); x != null; x = in.readLine()) {
+		        temps.add(x);
+		      }
+		    in.close();
+
+		return temps.toArray(new String[0]);
+
+		
+	}
+	
+	
+}
---
+++ graph-gen/src/main/java/com/thomsonreuters/graph/generator/TwoWayExclusiveRandomGenerator.java
@@ -0,0 +1,190 @@
+package com.thomsonreuters.graph.generator;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.graphstream.algorithm.generator.RandomGenerator;
+import org.graphstream.algorithm.util.RandomTools;
+import org.graphstream.graph.Node;
+
+public class TwoWayExclusiveRandomGenerator extends RandomGenerator {
+    // TODO: omg, hackathon refactor needed.
+	
+	private boolean canASelfLink = true;
+	
+	private boolean canBSelfLink = true;
+	
+	private List<String> aSet = new ArrayList<String>();
+	
+	private List<String> bSet = new ArrayList<String>();
+	
+	private int setAMaxSize = 100;
+	
+	private int setBMaxSize = 100;
+	
+	private String[] setAFile;
+	
+	private String[] setBFile;
+	
+	private double abRatio = new Double("0.50");
+	
+	static String ABATTR = "distribution";
+	static String IDENTIFIERATTR = "identifier";
+	
+	public TwoWayExclusiveRandomGenerator(double averageDegree, boolean allowRemove,
+			boolean directed, String edgeAttribute, Boolean canASelfLink, Boolean canBSelfLink, Double abRatio, String[] setAFile, String[] setBFile) {
+		super(averageDegree, allowRemove, directed, ABATTR,
+				edgeAttribute);
+		this.setUseInternalGraph(true);
+		this.canASelfLink = canASelfLink;
+		this.canBSelfLink = canBSelfLink;
+		this.setAFile = setAFile;
+		this.setBFile = setBFile;
+		this.setAMaxSize = setAFile.length;
+		this.setBMaxSize = setBFile.length;
+		this.abRatio = abRatio;
+	}
+
+	/**
+	 * Starts the generator. A clique of size equal to the average degree is
+	 * added.
+	 * 
+	 * @see org.graphstream.algorithm.generator.Generator#begin()
+	 * @complexity O(k<sup>2</sup>) where k is the average degree
+	 */
+	public void begin() {
+		Set<String> setMember;
+		if (allowRemove)
+			edgeIds = new ArrayList<String>();
+		subset = new HashSet<Integer>();
+		
+		String nodeId = null;
+		
+		for (nodeCount = 0; nodeCount <= (int) averageDegree; nodeCount++) {
+			nodeId=nodeCount + "";
+			double binaryValue;
+			double value = (random.nextDouble() * (nodeAttributeRange[1] - nodeAttributeRange[0])) + nodeAttributeRange[0];
+			
+			if (value < abRatio) 
+			{ 
+				if(aSet.size() < setAMaxSize) {
+					aSet.add(nodeId); 
+					binaryValue = 0;
+					addNodeWithDistributionAttribute(nodeId, binaryValue, setAFile[aSet.size() - 1]);
+					
+				} else { System.out.println("A size fail"); }
+			} 
+			else 
+			{ 
+				if(bSet.size() < setBMaxSize) {
+					bSet.add(nodeId); 
+					binaryValue = 1; 
+					addNodeWithDistributionAttribute(nodeId, binaryValue, setAFile[bSet.size() - 1]);
+				} else { System.out.println("B size fail"); }
+			}
+		}
+		
+	}
+
+
+	/**
+	 * If edge removing is allowed, removes a small fraction of the existing
+	 * edges. Then adds a new node and connects it randomly with some of the
+	 * existing nodes.
+	 * @return <code>true</code>
+	 * 
+	 * @see org.graphstream.algorithm.generator.Generator#nextEvents()
+	 * @complexity Each call of this method takes on average O(k) steps, where k
+	 *             is the average degree. Thus generating a graph with n nodes
+	 *             will take O(nk) time. The space complexity is O(nk) if edge
+	 *             removing is allowed and O(k) otherwise.
+	 */
+	public boolean nextEvents() {
+
+		System.out.println("cycle nodecount " + nodeCount);
+
+		
+		double binaryValue;
+		double value = (random.nextDouble() * (nodeAttributeRange[1] - nodeAttributeRange[0])) + nodeAttributeRange[0];
+		
+		if (value < abRatio) 
+		{ 
+			if(aSet.size() < setAMaxSize) {
+				if (allowRemove)
+					removeExistingEdges(1.0 / nodeCount);
+				
+				List<String> oppositeSet = null;
+				Boolean isSetA = null;
+				String nodeId=nodeCount + "";
+
+				double addProbability = averageDegree / nodeCount;
+				if(!allowRemove) addProbability /= 2; 
+				aSet.add(nodeId);
+				isSetA=true;  
+				oppositeSet = bSet; 
+				binaryValue = 0;
+				addNodeWithDistributionAttribute(nodeId, binaryValue, setAFile[aSet.size() - 1]);
+				
+				if((canASelfLink == false)) {
+					addNewEdgesExclusive(addProbability, oppositeSet, nodeId);
+				} else {
+					addNewEdges(addProbability);
+				}
+				
+				nodeCount++;
+				
+			} else { System.out.println("A size fail"); }
+		} 
+		else 
+		{ 
+			if(bSet.size() < setAMaxSize) {
+				if (allowRemove)
+					removeExistingEdges(1.0 / nodeCount);
+				
+				List<String> oppositeSet = null;
+				Boolean isSetA = null;
+				String nodeId=nodeCount + "";
+
+				double addProbability = averageDegree / nodeCount;
+				if(!allowRemove) addProbability /= 2; 
+				bSet.add(nodeId);
+				isSetA=false; 
+				oppositeSet = aSet; 
+				binaryValue = 1; 
+				addNodeWithDistributionAttribute(nodeId, binaryValue, setBFile[bSet.size() - 1]);
+				
+				if(canBSelfLink == false) {
+					addNewEdgesExclusive(addProbability, oppositeSet, nodeId);
+				} else {
+					addNewEdges(addProbability);
+				}
+				
+				nodeCount++;
+			} else { System.out.println("B size fail"); }
+		}
+		return true;
+	}
+	
+	protected void addNewEdgesExclusive(double p, List<String> coreSet, String nodeId) {
+		if(coreSet.size() > 0) { 
+			RandomTools.randomPsubset(coreSet.size(), p, subset, random); 
+		} 
+		else { return; }
+		for (int i : subset) {
+				String edgeId = coreSet.get(i) + "_" + nodeId;
+				addEdge(edgeId, coreSet.get(i) + "", nodeId);
+			if (allowRemove) edgeIds.add(edgeId);
+		}
+	}
+	
+	protected void addNodeWithDistributionAttribute(String nodeId, double binaryValue, String identifier) {
+		addNode(nodeId);
+		sendNodeAttributeAdded(sourceId, nodeId, ABATTR, binaryValue);
+		sendNodeAttributeAdded(sourceId, nodeId, IDENTIFIERATTR, identifier);
+		if (isUsingInternalGraph()) internalGraph.getNode(nodeId).addAttribute(ABATTR, binaryValue);
+		if(binaryValue==1 )	sendNodeAttributeAdded(sourceId, nodeId, "ui.class", "isSetB"); 
+	}	
+	
+}
